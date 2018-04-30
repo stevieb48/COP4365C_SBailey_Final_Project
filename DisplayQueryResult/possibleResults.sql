5aa7a8d314b10b4483f7fb356dacf13a80529804
stevieb48 <seb48@students.uwf.edu>
2018-04-30 17:15:55 -0500

    possible results queries

--- DisplayQueryResult/possibleResults.sql
+++ DisplayQueryResult/possibleResults.sql
@@ -2,13 +2,12 @@
 FROM Titles AS T
 INNER JOIN AuthorISBN as X ON T.ISBN = X.ISBN
 INNER JOIN Authors as A ON X.AuthorID = A.AuthorID
-ORDER BY T.Title;
+ORDER BY T.Title ASC;
 
 SELECT T.Title, A.FirstName, A.LastName
 FROM Titles AS T
 INNER JOIN AuthorISBN as X ON T.ISBN = X.ISBN
 INNER JOIN Authors as A ON X.AuthorID = A.AuthorID
-GROUP BY T.Title, A.LastName, A.FirstName
 ORDER BY T.Title, A.LastName ASC, A.FirstName ASC;
 
 SELECT A.FirstName, A.LastName, T.Title
